54ae6cb9f1e255d2fe50fd2f506b697492818f6b
saade <paul.saade@efrei.net>
2025-09-05 15:56:16 +0200

    Changing Box

--- AR.js
+++ AR.js
@@ -1,12 +1,28 @@
-// Component to change to a sequential color on click.
-AFRAME.registerComponent('cursor-listener', {
-  init: function () {
-    var lastIndex = -1;
-    var COLORS = ['red', 'green', 'blue'];
-    this.el.addEventListener('click', function (evt) {
-      lastIndex = (lastIndex + 1) % COLORS.length;
-      this.setAttribute('material', 'color', COLORS[lastIndex]);
-      console.log('I was clicked at: ', evt.detail.intersection.point);
-    });
-  }
-});
+      // Récupère la boîte
+      const box = document.querySelector('#myBox');
+
+      // Rends la boîte "cliquable"
+      box.classList.add('clickable');
+
+      // Ajoute un écouteur de clic
+      box.addEventListener('click', function () {
+        // Change de couleur aléatoire
+        const colors = ['#EF2D5E', '#4CC3D9', '#FFC65D', '#7BC8A4', '#F0F000'];
+        this.setAttribute('color', colors[Math.floor(Math.random() * colors.length)]);
+        
+        // Ajoute une animation supplémentaire (scale ou rotation rapide)
+        this.setAttribute('animation__scale', {
+          property: 'scale',
+          to: '2 2 2',
+          dur: 500,
+          dir: 'alternate',
+          loop: 1
+        });
+
+        this.setAttribute('animation__rotatefast', {
+          property: 'rotation',
+          to: '0 720 0',
+          dur: 1000,
+          loop: 1
+        });
+      });
